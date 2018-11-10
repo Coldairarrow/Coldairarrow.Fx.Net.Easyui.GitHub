61011920f93cb0f61f620eac5b95e99d3d776dd0
Coldairarrow <xgc15657519397@outlook.com>
2018-11-10 19:15:33 +0800

    RPC框架开发

---
+++ src/Coldairarrow.Util/ClassLibrary/RPC/RPCClientFactory.cs
@@ -0,0 +1,12 @@
+﻿using ImpromptuInterface;
+
+namespace Coldairarrow.Util.RPC
+{
+    public class RPCClientFactory
+    {
+        public static T GetClient<T>() where T : class
+        {
+            return new RPCClientProxy().ActLike<T>();
+        }
+    }
+}
---
+++ src/Coldairarrow.Util/ClassLibrary/RPC/RPCClientProxy.cs
@@ -0,0 +1,21 @@
+﻿using System.Dynamic;
+
+namespace Coldairarrow.Util.RPC
+{
+    class RPCClientProxy: DynamicObject
+    {
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+        {
+            try
+            {
+                result = "厉害了我的哥";
+                return true;
+            }
+            catch
+            {
+                result = null;
+                return false;
+            }
+        }
+    }
+}
