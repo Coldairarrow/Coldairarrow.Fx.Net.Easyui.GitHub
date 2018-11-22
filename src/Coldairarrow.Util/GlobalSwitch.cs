f5ce5f7d5ce0d08fbc284cbcdf6468f76b9ce554
Coldairarrow <xgc15657519397@outlook.com>
2018-11-22 20:28:23 +0800

    更新

--- src/Coldairarrow.Util/GlobalSwitch.cs
+++ src/Coldairarrow.Util/GlobalSwitch.cs
@@ -35,7 +35,7 @@
         /// <summary>
         /// 运行模式
         /// </summary>
-        public static RunModel RunModel { get; } = RunModel.Publish;
+        public static RunModel RunModel { get; } = RunModel.LocalTest;
 
         #endregion
 
