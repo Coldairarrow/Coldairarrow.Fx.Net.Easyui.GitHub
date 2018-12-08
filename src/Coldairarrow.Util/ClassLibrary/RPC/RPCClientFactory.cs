1e70f61da606faad05fb92e135ba2c7ac87b6920
Coldairarrow <xgc15657519397@outlook.com>
2018-12-08 22:40:08 +0800

    添加RPC框架

--- src/Coldairarrow.Util/ClassLibrary/RPC/RPCClientFactory.cs
+++ src/Coldairarrow.Util/ClassLibrary/RPC/RPCClientFactory.cs
@@ -4,9 +4,17 @@ namespace Coldairarrow.Util.RPC
 {
     public class RPCClientFactory
     {
-        public static T GetClient<T>() where T : class
+        public static T GetClient<T>(string serverIp, int port) where T : class
         {
-            return new RPCClientProxy().ActLike<T>();
+            var clientProxy = new RPCClientProxy
+            {
+                _serverIp = serverIp,
+                _serverPort = port,
+                ServiceType = typeof(T),
+                ServiceName = typeof(T).FullName
+            };
+
+            return clientProxy.ActLike<T>();
         }
     }
 }
