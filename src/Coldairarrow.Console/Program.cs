6683c7b77920b31e8e6a7c596844a2c083f4b348
Coldairarrow <xgc15657519397@outlook.com>
2019-01-14 00:55:43 +0800

    更新

--- src/Coldairarrow.Console/Program.cs
+++ src/Coldairarrow.Console/Program.cs
@@ -257,21 +257,9 @@ namespace Coldairarrow.Console1
             //DotNettyTest();
             //RpcTest();
             //DotNettyRPCTest();
-
-
-            var bootstrap = new Bootstrap()
-                .Group(new MultithreadEventLoopGroup())
-                .Channel<TcpSocketChannel>()
-                .Option(ChannelOption.TcpNodelay, true)
-                .Handler(new ActionChannelInitializer<ISocketChannel>(channel =>
-                {
-                    IChannelPipeline pipeline = channel.Pipeline;
-                    pipeline.AddLast("framing-enc", new LengthFieldPrepender(2));
-                    pipeline.AddLast("framing-dec", new LengthFieldBasedFrameDecoder(ushort.MaxValue, 0, 2, 0, 2));
-
-                    //pipeline.AddLast(new EchoClientHandler(clientWait));
-                }));
-            var clientChannel = bootstrap.ConnectAsync($"61.174.68.148:3004".ToIPEndPoint()).Result;
+            RPCServer rpcServer = new RPCServer(9999);
+            rpcServer.RegisterService<IHello, Hello>();
+            rpcServer.Start();
 
             Console.WriteLine("完成");
             Console.ReadLine();
