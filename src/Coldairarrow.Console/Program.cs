263a22e4588bd0eab250bc7146662d3b6d42156f
Coldairarrow <xgc15657519397@outlook.com>
2019-01-31 11:15:33 +0800

    更新

--- src/Coldairarrow.Console/Program.cs
+++ src/Coldairarrow.Console/Program.cs
@@ -6,7 +6,6 @@ namespace Coldairarrow.Console1
     {
         static void Main(string[] args)
         {
-            string tmp = string.Empty;
             Console.WriteLine("完成");
             Console.ReadLine();
         }
